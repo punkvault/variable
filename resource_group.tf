dd292d13c3675a2b4cbee18fd59f0f6eded1c8e8
Pankaj Kumar <pankaj.kumar660@gmail.com>
2026-05-01 00:41:07 +0530

    Deleted all repo contents

--- resource_group.tf
+++
@@ -1,6 +0,0 @@
-
-
-resource "azurerm_resource_group" "month" {
-  name     = var.rg_name    #variable use
-  location = var.rg_location
-}
--- variable.tf
+++
@@ -1,6 +0,0 @@
-variable "rg_name" {
-    default = "rg-July"
-}      #variable declare
-variable "rg_location" {
-    default = "westus"
-}
